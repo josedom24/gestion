11b66fb87744c52926b6d449aac39a2c782b2e63
josedom24 <josedom24@gmail.com>
2015-05-06 22:30:15 +0200

    Centro

--- centro/models.py
+++ centro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Cursos(models.Model):
+	
+	Curso = models.CharField(max_length=30)
+
+	def __unicode__(self):
+		return self.Curso
+
+	class Meta:
+		verbose_name="Curso"
+		verbose_name_plural="Cursos"
