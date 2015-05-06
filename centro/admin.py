904de0a700b9c7bdccbe9d9ac6d70e33503aa265
josedom24 <josedom24@gmail.com>
2015-05-06 22:47:20 +0200

    Centro

--- centro/admin.py
+++ centro/admin.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from centro.models import Cursos
+from centro.models import Cursos,Alumnos
 
 # Register your models here.
 admin.site.site_header="Gonzalo Nazareno"
 admin.site.index_title="Gestión amonestaciones"
 admin.site.register(Cursos)
+admin.site.register(Alumnos)
--- centro/models.py
+++ centro/models.py
@@ -12,3 +12,30 @@ class Cursos(models.Model):
 	class Meta:
 		verbose_name="Curso"
 		verbose_name_plural="Cursos"
+
+class Alumnos(models.Model):
+	Nombre = models.CharField(max_length=50)
+	DNI = models.CharField(max_length=9)
+	Direccion = models.CharField(max_length=60)
+	CodPostal = models.CharField(max_length=5,verbose_name="Código postal")
+	Localidad = models.CharField(max_length=30)
+	Fecha_nacimiento = models.DateField('Fecha de nacimiento')
+	Provincia = models.CharField(max_length=30)
+	Unidad = models.ForeignKey(Cursos)
+	Ap1tutor = models.CharField(max_length=20,verbose_name="Apellido 1 tutor")
+	Ap2tutor = models.CharField(max_length=20,verbose_name="Apellido 2 tutor")
+	Nomtutor = models.CharField(max_length=20,verbose_name="Nombre tutor")
+	Telefono1 = models.CharField(max_length=12,blank=True)
+	Telefono2 = models.CharField(max_length=12,blank=True)
+	Obs=models.TextField(blank=True,verbose_name="Observaciones")
+
+	def __unicode__(self):
+		return self.DNI+" - "+self.Nombre 
+
+	def get_absolute_url(self):
+		return reverse('server_edit', kwargs={'pk': self.pk})
+
+
+	class Meta:
+		verbose_name="Alumno"
+		verbose_name_plural="Alumnos"
