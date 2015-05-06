c4b6f5bcf05dbf1be5639b1cc4b1bb3a771e5887
josedom24 <josedom24@gmail.com>
2015-05-06 22:32:19 +0200

    Centro

--- centro/admin.py
+++ centro/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from centro.models import Cursos
 
 # Register your models here.
+admin.site.site_header="Gonzalo Nazareno"
+admin.site.index_title="Gestión amonestaciones"
+admin.site.register(Cursos)
