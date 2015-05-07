cbf6b25e1f33a5d2b986011f2bd2dad5e8831d9a
josedom24 <josedom24@gmail.com>
2015-05-07 14:17:42 +0200

    Convivencia

--- convivencia/admin.py
+++ convivencia/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from convivencia.models import Amonestaciones
 
 admin.site.register(Amonestaciones)
+admin.site.register(FaltasLeves)
+admin.site.register(FaltasGraves)
