4bd620d6da31c3d39d056bba0e8139af427995a4
josedom24 <josedom24@gmail.com>
2016-10-26 20:12:31 +0200

    Modelo

--- gestion/settings.py
+++ gestion/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'centro.apps.ProyectoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
