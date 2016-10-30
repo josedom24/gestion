72c508005c303cfdfebfdd782428fffbba9f13b9
josedom24 <josedom24@gmail.com>
2016-10-30 20:17:54 +0100

    Listados

--- pdf/urls.py
+++ pdf/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^partes/(?P<curso>[0-9]+)$', views.imprimir_partes),
-	
+	url(r'^show/(?P<tipo>[a-z]+)/(?P<mes>[0-9]+)/(?P<ano>[0-9]+)/(?P<dia>[0-9]+)$', views.imprimir_show),
+	url(r'^carta_amonestacion/(?P<mes>[0-9]+)/(?P<ano>[0-9]+)/(?P<dia>[0-9]+)$', views.carta_amonestacion),
+	url(r'^carta_sancion/(?P<identificador>[0-9]+)$', views.carta_sancion),
 ]
--- pdf/views.py
+++ pdf/views.py
@@ -12,7 +12,10 @@ from django.contrib.auth.decorators import login_required
 from xhtml2pdf import pisa
 from cStringIO import StringIO
 from centro.models import Alumnos,Cursos
+from convivencia.models import Amonestaciones,Sanciones
 from centro.views import ContarFaltas
+
+from datetime import datetime
 
 
 # Create your views here.
@@ -24,9 +27,54 @@ def imprimir_partes(request,curso):
 	lista=zip(range(1,len(lista_alumnos)+1),lista_alumnos,ContarFaltas(lista_alumnos.values("id")))
 	data={'alumnos':lista,'curso':Cursos.objects.get(id=curso)}
 	# Render html content through html template with context
-	return imprimir("partes.html",data,"partes.pdf")
+	return imprimir("pdf_partes.html",data,"partes.pdf")
 
 
+@login_required(login_url='/')
+def imprimir_show(request,tipo,mes,ano,dia):
+	fecha=datetime(int(ano),int(mes),int(dia))
+	if tipo=="amonestacion":
+		datos=Amonestaciones.objects.filter(Fecha=fecha)
+		titulo="Resumen de amonestaciones"
+	if tipo=="sancion":
+		datos=Sanciones.objects.filter(Fecha=fecha)
+		titulo="Resumen de sanciones"
+	
+	datos=zip(range(1,len(datos)+1),datos,ContarFaltas(datos.values("IdAlumno")))
+	data={'datos':datos,'tipo':tipo,'fecha':fecha,'titulo':titulo,tipo:True}
+	return imprimir("pdf_resumen.html",data,"resumen_"+tipo+".pdf")	
+
+@login_required(login_url='/')
+def carta_amonestacion(request,mes,ano,dia):
+	info={}
+	contenido=""
+	fecha2=datetime(int(ano),int(mes),int(dia))
+	info["fecha"]="%s/%s/%s"%(dia,mes,ano)
+	info["amonestaciones"]=Amonestaciones.objects.filter(Fecha=fecha2)
+
+	for i in info["amonestaciones"]:
+		info2={}
+		info2["amonestacion"]=i
+		info2["num_amon"]=str(len(Amonestaciones.objects.filter(IdAlumno_id=i.IdAlumno_id)))
+		template = get_template("pdf_contenido_carta_amonestacion.html")
+		contenido=contenido+ template.render(Context(info2))
+		if i.id!=info["amonestaciones"].last().id:
+			contenido=contenido+"<pdf:nextpage>"
+	info["contenido"]=contenido
+	return imprimir("pdf_carta.html",info,"carta_amonestacion"+".pdf")	
+	
+
+
+@login_required(login_url='/')
+def carta_sancion(request,identificador):
+	info2={}
+	contenido=""
+	
+	info2["sancion"]=Sanciones.objects.get(id=identificador)
+	info={}
+	template = get_template("pdf_contenido_carta_sancion.html")
+	info["contenido"]=template.render(Context(info2))	
+	return imprimir("pdf_carta.html",info,"carta_sancion"+".pdf")	
 
 
 	
