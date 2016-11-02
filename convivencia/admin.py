cbefc7d5af5d951cb525fecf5d04a1d780e2cf91
josedom24 <josedom24@gmail.com>
2016-11-02 17:57:38 +0100

    Equipo educativo

--- convivencia/admin.py
+++ convivencia/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from convivencia.models import Amonestaciones,Sanciones
 # Register your models here.
 
-admin.site.register(Amonestaciones)
-admin.site.register(Sanciones)
+
+class AmonestacionesAdmin(admin.ModelAdmin):
+    #date_hierarchy = 'Fecha_nacimiento'
+    actions_selection_counter=False
+    list_filter = ['Fecha','Profesor']
+    list_display = ["Fecha","IdAlumno","Comentario"]
+     
+    search_fields = ['Comentario']
+
+class SancionesAdmin(admin.ModelAdmin):
+    #date_hierarchy = 'Fecha_nacimiento'
+    actions_selection_counter=False
+    list_filter = ['Fecha']
+    list_display = ["Fecha","Fecha_fin","IdAlumno","Sancion"]
+     
+    search_fields = ['Comentario']
+
+admin.site.register(Amonestaciones,AmonestacionesAdmin)
+admin.site.register(Sanciones,SancionesAdmin)
