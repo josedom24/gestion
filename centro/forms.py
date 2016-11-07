ef8f2480efda64293b9dac2e1fcc6c7099143b52
josedom24 <josedom24@gmail.com>
2016-11-07 21:26:17 +0100

    Quito buscar

--- centro/forms.py
+++ centro/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from centro.models import Cursos,Departamentos,Areas
 
 class UnidadForm(forms.Form):
-	Unidad = forms.ModelChoiceField(queryset=Cursos.objects.order_by('Curso'), empty_label=None,widget=forms.Select(attrs={'class': "form-control"}))
+	Unidad = forms.ModelChoiceField(queryset=Cursos.objects.order_by('Curso'), empty_label=None,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
 
 class DepartamentosForm(forms.Form):
         Areas = forms.ModelChoiceField(queryset=Areas.objects.all(),required=False,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
-        Departamento = forms.ModelChoiceField(queryset=Departamentos.objects.order_by('Nombre'), required=False,widget=forms.Select(attrs={'class': "form-control"}))
+        Departamento = forms.ModelChoiceField(queryset=Departamentos.objects.order_by('Nombre'), required=False,widget=forms.Select(attrs={'class': "form-control",'onchange': 'this.form.submit();'}))
 
         def __init__(self, *args, **kwargs):
             super(DepartamentosForm, self).__init__(*args, **kwargs)
