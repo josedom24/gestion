08894aefea29eb65d16a9db4800cdf338a5ede9c
josedom24 <josedom24@gmail.com>
2016-11-16 17:01:24 +0100

    Combo ordenados

--- pdf/urls.py
+++ pdf/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^partes/(?P<curso>[0-9]+)$', views.imprimir_partes),
+	url(r'^faltas/(?P<curso>[0-9]+)$', views.imprimir_faltas),
 	url(r'^historial/(?P<alum_id>[0-9]+)$', views.imprimir_historial),
 	url(r'^show/(?P<tipo>[a-z]+)/(?P<mes>[0-9]+)/(?P<ano>[0-9]+)/(?P<dia>[0-9]+)$', views.imprimir_show),
 	url(r'^carta_amonestacion/(?P<mes>[0-9]+)/(?P<ano>[0-9]+)/(?P<dia>[0-9]+)$', views.carta_amonestacion),
