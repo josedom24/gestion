946abb780c89de66f5209a1277be0685b0f20690
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2019-09-16 18:13:11 +0200

    mis alumnos

--- centro/views.py
+++ centro/views.py
@@ -65,8 +65,8 @@ def misalumnos(request):
 		
 
         try:
-    	    print(buscarcurso)
-	    primer_id = Cursos.objects.get(Curso=buscarcurso).id
+
+	    primer_id = Cursos.objects.get(Curso=buscarcurso.encode('utf-8')).id
         except:
             return redirect("/")
 	request.session['Unidad']=primer_id
